8a41bb22eb0aac876c7d6ff0560ac316bf5d2779
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-01-03 23:45:15 +0500

    Column Remove

---
+++ src/Questions/Remove_Column.java
@@ -0,0 +1,22 @@
+package Questions;
+
+public class Remove_Column {
+    public int minDeletionSize(String[] strs) {
+        int n = strs.length;
+        int k = strs[0].length();
+
+        int count = 0;
+
+        for (int i = 0; i < k; i++) {
+            for (int j = 1; j < n; j++) {
+                if (strs[j].charAt(i) < strs[j - 1].charAt(i)) {
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+
+    }
+}
