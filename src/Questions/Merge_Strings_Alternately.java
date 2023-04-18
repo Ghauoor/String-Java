db17de3c3375d7d5f597e8cfb0d52962a8a53487
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-19 01:53:53 +0500

    1768. Merge Strings Alternately

---
+++ src/Questions/Merge_Strings_Alternately.java
@@ -0,0 +1,18 @@
+package Questions;
+
+public class Merge_Strings_Alternately {
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder res = new StringBuilder();
+
+        for (int i = 0; i < word1.length() || i < word2.length(); ++i) {
+            if (i < word1.length()) {
+                res.append(word1.charAt(i));
+            }
+            if (i < word2.length()) {
+                res.append(word2.charAt(i));
+            }
+        }
+
+        return res.toString();
+    }
+}
